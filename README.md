e5194c170ee9273dbec030ad490b6834e3143b97
marc borkowski <borkowskiberlin@gmail.com>
2018-07-23 21:43:29 +0200

    im working alone... so i dont care

--- README.md
+++ README.md
@@ -2,14 +2,18 @@
 
 Simple Script to check Referrer in Logfile. Json File is hardcoded... muhahahah 2nd Line @main.js.
 
-#requirement
+# requirement
 - Python (windows: https://www.python.org/downloads/windows/)
 - Node (https://nodejs.org/en/)
 
+# installation
+- npm install
+
+# step by step
 1. save logfile in the same folder
 2. python main.py -f [filename]
 3. ~~edit main.js 2nd Line (not necessary cause python output is the same as hardcoded input)~~
 4. node main.js
 
-#Version
+# Version
 Alpha 0.0
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-fs = require('fs');
+var fs = require('fs');
+var Table = require('cli-table3');
+
 fs.readFile('log.json', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
@@ -6,30 +8,52 @@ fs.readFile('log.json', 'utf8', function (err,data) {
 
   data = JSON.parse(data);
   var regex_search = RegExp('^.*(google|bing).[a-z]*.*$');
-  var regex_social = RegExp('^.*https://t.co*.*$');
 
-  function analyse(regex){
-    var regex_uploadDir = RegExp('^GET /wp-content/uploads/.*$');
+  // instantiate
+  var table_search = new Table({
+      head: ['Hits', 'URL']
+  });
+
+  var regex_social = RegExp('^.*https://t.co*.*$');
+  // instantiate
+  var table_social = new Table({
+      head: ['Hits', 'URL']
+  });
+
+  function analyse(regex, table){
+    var regex_uploadDir = RegExp('^GET (/wp-content/|/sandbox/).*$');
 
     let counter = 1;
     let score = {all: 0}
     while(typeof data[counter] !== 'undefined'){
       if(regex.test(data[counter].REFERER) && !regex_uploadDir.test(data[counter].REQUEST) ){
-        if(typeof score[data[counter].REQUEST] !== 'undefined'){
-          score[data[counter].REQUEST]++;
+        let req = data[counter].REQUEST.replace('GET ', '');
+        req = req.replace(' HTTP/1.1', '');
+        if(typeof score[req] !== 'undefined'){
+          score[req]++;
           score.all++;
         }
         else{
-          score[data[counter].REQUEST] = 1;
+          score[req] = 1;
           score.all++;
         }
       }
       counter++
     }
 
-    return score;
-  }
+    for(let key in score){
+      table.push([key, score[key]]);
+    }
+    table.sort(function(a, b) {
+        return b[1] - a[1];
+    });
 
-  console.log(analyse(regex_search));
-  console.log(analyse(regex_social));
+    return table;
+  }
+  console.log("# Google & Bing ###")
+  table_search = analyse(regex_search, table_search);
+  console.log(table_search.toString());
+  table_social = analyse(regex_social, table_social);
+  console.log("# Twitter ###")
+  console.log(table_social.toString());
 });
